606d445ae0c89e010896919b1d35941c1a9cb5e2
pi-art-314 <devinsmith95@gmail.com>
2025-02-12 14:52:32 -0500

    Added classes CircleShape & Player for drawing these shapes. Modified constants (added default radius). Modified main to draw player object on display.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 from constants import *
+from player import *
+
 def main():
 	pygame.init
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	test_player = Player(x, y)
+	game_clock = pygame.time.Clock()
+	dt = 0
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		test_player.draw(screen)
 		pygame.display.flip()
+		dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
